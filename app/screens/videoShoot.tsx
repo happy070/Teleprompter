24ac68b48179ba6595284c577fe717978a429d41
Himanshu Joshi <himanshu@bytive.in>
2025-06-11 23:45:41 +0530

    video page

--- app/screens/videoShoot.tsx
+++ app/screens/videoShoot.tsx
@@ -1,14 +1,52 @@
-
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const videoShoot = () => {
-
+const VideoShoot = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>happy</Text>
+    <View style={styles.container}>
+      <View style={styles.videoContainer}>
+           <View style={[styles.videoView, styles.aspectRatio16_9]}>
+          <Text style={styles.text}>Scrollable Text</Text>
+        </View>
+        <View style={[styles.videoView, styles.aspectRatio9_16]}>
+          <Text style={styles.text}>Live Video</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
-export default videoShoot;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  videoContainer: {
+    width: '95%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  videoView: {
+    backgroundColor: '#000',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+    width: '100%',
+  },
+  aspectRatio9_16: {
+    aspectRatio: 9 / 11,
+  },
+  aspectRatio16_9: {
+    aspectRatio: 16 / 9,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default VideoShoot;
